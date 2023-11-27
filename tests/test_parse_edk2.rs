19592aa6ba873e75e2a327391ba90e600c1ee20c
Joey Vagedes <joey.vagedes@gmail.com>
2023-11-26 19:49:21 -0800

    add tests that parse edk2 (#5)
    Adds a new test file for parsing edk2 itself. This test is to ensure
    any changes do not any config parser as a whole when run against edk2.
    
    This not does mean the parsers continues to be correct. Other tests are
    necessary for that. This only means that any changes do not break the
    ability to parse in general.
    
    This is a work in progress as only the inf parser is working

---
+++ tests/test_parse_edk2.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    use edk2_parser::{inf::Inf, ConfigParser};
+
+    #[test]
+    fn parse_edk2_inf() {
+        let repo_path = std::env::current_dir()
+            .unwrap()
+            .join("target")
+            .join("tmp")
+            .join("edk2");
+        if !repo_path.exists() {
+            git2::Repository::clone("https://github.com/tianocore/edk2.git", &repo_path)
+                .unwrap()
+                .workdir()
+                .unwrap();
+        }
+
+        let pattern = format!("{}/**/*.{}", repo_path.display(), "inf");
+        println!("pattern: {}", pattern);
+        glob::glob(pattern.as_str()).unwrap().for_each(|entry| {
+            let mut parser = ConfigParser::<Inf>::new();
+            let path = entry.unwrap();
+            parser.parse_from_file(path).unwrap();
+        });
+    }
+}
