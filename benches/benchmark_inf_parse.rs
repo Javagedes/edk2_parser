c9027d53a907ca08d95533cc954d5e258fafa4b0
= <joey.vagedes@gmail.com>
2023-11-26 16:18:57 -0800

    add benchmarking to github actions
    Adds the first benchmark, benchmark_inf_parse.rs to the codebase and
    creates a github action to run benchmarks on every pull request and
    push to the main branch utilzing the criterion crate and the
    github-action-benchmark action.

---
+++ benches/benchmark_inf_parse.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate criterion;
+
+use std::time::Duration;
+
+use criterion::Criterion;
+use edk2_parser::{ConfigParser, inf::Inf};
+
+// The function you want to benchmark
+fn parse_inf(data: String) {
+    let mut parser = ConfigParser::<Inf>::new();
+    parser.parse(data).unwrap();
+}
+
+// Criterion benchmark group
+fn my_benchmark(c: &mut Criterion) {
+    let d1 = include_str!("../tests/data/baseLib.inf").to_string();
+    let d2 = include_str!("../tests/data/opensslLib.inf").to_string();
+    
+    let mut group = c.benchmark_group("parse_inf");
+    group.sample_size(500);
+    group.measurement_time(Duration::from_secs(120));
+    group.bench_function("baseLib", |b| b.iter(|| parse_inf(d1.clone())));
+    group.bench_function("opensslLib", |b| b.iter(|| parse_inf(d2.clone())));
+}
+
+criterion_group!(benches, my_benchmark /*, another_benchmark*/);
+criterion_main!(benches);
