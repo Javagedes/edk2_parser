43b38c9528d2cfb9cb06883f9edac7a909abf0e6
= <joey.vagedes@gmail.com>
2023-11-27 21:46:32 -0800

    update

--- src/err.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum ParseError {
-    #[error("Invalid Format. Expected [{0}]")]
-    InvalidFormat(String),
-    #[error("Unknown Section: [{0}]")]
-    UnknownSection(String),
-}
